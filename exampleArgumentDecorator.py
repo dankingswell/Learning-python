c3342fb0180724370d4e53195674919e401466a0
Dan Kingswell <dankingswell22@gmail.com>
2018-04-09 10:58:30 +0100

    decorators with notes

--- exampleArgumentDecorator.py
+++ exampleArgumentDecorator.py
@@ -5,6 +5,7 @@ class decoratorWithArguments(object):
         If there are decorator arguments, the function
         to be decorated is not passed to the constructor!
         """
+        # the function is called with the given decorator arguments and saves them in the object
         print ("Inside __init__()")
         self.arg1 = arg1
         self.arg2 = arg2
@@ -16,6 +17,10 @@ class decoratorWithArguments(object):
         once, as part of the decoration process! You can only give
         it a single argument, which is the function object.
         """
+        # when the decorator is initalised it is passed the function below through decoratorwitharguments __call__(self SayHello)
+        # this is then wrapped up as below and this then forms the new call method
+        # when the decorated method is then called the inner wrapped function is the __call__method due to the above re assignment and is given
+        # scope to see the arguments through the objects earlier save
         print ("Inside __call__()")
         def wrapped_f(*args):
             print ( "Inside wrapped_f()")
